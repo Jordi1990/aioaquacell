0452676b697166e3b2654bbbe5dc070bfd6bdcb5
Jordi Epema <jordi.epema@gmail.com>
2023-10-03 21:31:56 +0200

    Fixed attempt to parse refresh token when not received after a refresh

--- aioaquacell/authentication_tokens.py
+++ aioaquacell/authentication_tokens.py
@@ -4,5 +4,8 @@
 class AuthenticationTokens:
     def __init__(self, data):
         self.id_token = data["IdToken"]
-        self.refresh_token = data["RefreshToken"]
         self.access_token = data["AccessToken"]
+        try:
+            self.refresh_token = data["RefreshToken"]
+        except KeyError:
+            self.refresh_token = None
